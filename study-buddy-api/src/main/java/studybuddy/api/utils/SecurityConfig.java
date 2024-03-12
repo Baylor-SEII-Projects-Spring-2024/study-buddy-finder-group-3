073b5b40b6b912345f98726ea12bb0dd3eb670d1
AlexBaratta <alexbaratta1@gmail.com>
2024-03-12 15:32:00 -0500

    cicd fix maybe

--- study-buddy-api/src/main/java/studybuddy/api/utils/SecurityConfig.java
+++ study-buddy-api/src/main/java/studybuddy/api/utils/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://34.16.138.129:3000/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
