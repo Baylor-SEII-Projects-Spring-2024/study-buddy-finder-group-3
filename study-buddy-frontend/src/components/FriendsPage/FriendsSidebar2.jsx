999b784c8fe6ae6541201655dd480f5cff6accd8
Wesley Anastasi <anastasicrl@gmail.com>
2024-03-19 17:32:12 -0500

    added styling to slider

--- study-buddy-frontend/src/components/FriendsPage/FriendsSidebar2.jsx
+++ study-buddy-frontend/src/components/FriendsPage/FriendsSidebar2.jsx
@@ -1,14 +1,17 @@
 import React from "react"
 import Box from "@mui/material/Box"
 import FriendsHeader from "./FriendsHeader"
-import { Drawer, IconButton, Typography } from "@mui/material"
+import { Drawer, IconButton, ListItemIcon, Typography } from "@mui/material"
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import { Button } from "react-scroll";
 import PeopleIcon from '@mui/icons-material/People';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import PersonOffIcon from '@mui/icons-material/PersonOff';
 import ForumIcon from '@mui/icons-material/Forum';
-import Slide from "@mui/material/slide";
+import Slide from "@mui/material/Slide";
+import FastRewindOutlinedIcon from '@mui/icons-material/FastRewindOutlined';
+import { useEffect } from "react";
+import { ListItemButton, ListItemText, List } from "@mui/material";
 
 
 export default function FriendsSidebar2() {
@@ -27,34 +30,86 @@ export default function FriendsSidebar2() {
         {button !== 'expand' ? setActivePage(button) : null}
     }
 
+    useEffect(() => {
+        const updateHeight = () => {
+            if (containerRef.current) {
+                const padding = 65; // Adjust this value according to your padding
+                const screenHeight = window.innerHeight;
+                const maxHeight = screenHeight - padding;
+
+                containerRef.current.style.height = `${maxHeight}px`;
+            }
+        };
+
+        updateHeight();
+        window.addEventListener('resize', updateHeight);
+        return () => window.removeEventListener('resize', updateHeight);
+    }, []);
+
 
     return (
-        <Box sx={{ display: "flex", flexDirection: "row", width: "100%", height: "100vh" }}>
-            <Box sx={{ width: "70px", minHeight: "100vh", borderRight: "1px solid black", zIndex: (theme) => theme.zIndex.drawer + 1 }} >
-                <IconButton sx={{marginTop: "70px"}} onClick={() => handleButtonClick('expand')}>
-                    <MenuOpenIcon/>
+        <Box sx={{ display: "flex", flexDirection: "row", width: "100%", height: "100vh", flex: "1" }}>
+            <Box sx={{ width: "50px", minHeight: "100vh", borderRight: "1px solid black", marginLeft: 1 }} >
+                <IconButton sx={{marginTop: "80px", marginBottom: "10px"}} onClick={() => handleButtonClick('expand')}>
+                    <MenuOpenIcon sx={{ color: "black" }}/>
                 </IconButton>
-                <IconButton onClick={() => handleButtonClick('list')}>
+                <IconButton onClick={() => handleButtonClick('list')} sx={{marginTop: '2px'}}>
                     <PeopleIcon/>
                 </IconButton>
-                <IconButton onClick={() => handleButtonClick('requests')}>
+                <IconButton onClick={() => handleButtonClick('requests')} sx={{marginTop: '10px'}}>
                     <PersonAddIcon/>
                 </IconButton>
-                <IconButton onClick={() => handleButtonClick('blocked')}>
+                <IconButton onClick={() => handleButtonClick('blocked')} sx={{marginTop: '6px'}}>
                     <PersonOffIcon/>
                 </IconButton>
-                <IconButton onClick={() => handleButtonClick('chat')}>
+                <IconButton onClick={() => handleButtonClick('chat')} sx={{marginTop: '8px'}}>
                     <ForumIcon/>
                 </IconButton>
             </Box> 
-            <Box sx={{ display: "flex", flexDirection: "column", flex: "1", height: "100%" }}>
-                <FriendsHeader/>
-                <Slide direction="right" in={open}>
-                    <IconButton onClick={() => handleButtonClick('expand')}>
-                        <MenuOpenIcon/>
-                    </IconButton>
-                </Slide>
+            <Box sx={{ display: "flex", flexDirection: "column", height: "100%", flex: "1" }}>
                 
+                <FriendsHeader/>
+                <Box style={{position: 'absolute', marginTop: "65px" , overflow:'hidden'}} ref={containerRef}>
+                    <Slide direction="right" in={open} container={containerRef.current}>
+                        <Box sx={{ display: "flex", flexDirection: "column", height: "100%", flex: "1", borderRight: "1px solid black"}}>
+                            <Box sx={{backgroundColor: "white", display: 'flex', justifyContent: 'center', marginBottom: "", paddingTop: "15px", paddingRight: 1, paddingLeft: 1 }}>
+                                <Typography variant="h6" fontWeight="bold" sx={{marginTop: "4px"}}>
+                                    Friends
+                                </Typography>
+                                <IconButton onClick={() => handleButtonClick('expand')} >
+                                <FastRewindOutlinedIcon sx={{color: "black" }}/>
+                                </IconButton>
+                            </Box>
+                            <List sx={{paddingRight: 1, paddingLeft: 1 }}>
+                                <ListItemButton onClick={() => handleButtonClick('list')}>
+                                    <ListItemIcon>
+                                        <PeopleIcon/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="All" />
+                                </ListItemButton>
+                                <ListItemButton onClick={() => handleButtonClick('requests')}>
+                                    <ListItemIcon>
+                                        <PersonAddIcon/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Requests" />
+                                </ListItemButton>
+                                <ListItemButton onClick={() => handleButtonClick('blocked')}>
+                                    <ListItemIcon>
+                                        <PersonOffIcon/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Blocked" />
+                                </ListItemButton>
+                                <ListItemButton onClick={() => handleButtonClick('chat')}>
+                                    <ListItemIcon>
+                                        <ForumIcon/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Chat" />
+                                </ListItemButton>
+                            </List>
+                        </Box>
+                        
+                    </Slide>
+                </Box>
                 {activePage === 'list' ? <Box>Friends</Box> : null}
                 {activePage === 'requests' ? <Box>Requests</Box> : null}
                 {activePage === 'blocked' ? <Box>Blocked</Box> : null}
