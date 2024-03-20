edd722361f7d7221f515d67039d0407ed9e6a0e5
AlexBaratta <alexbaratta1@gmail.com>
2024-03-19 22:23:38 -0500

    basic styling + locoscroll

--- study-buddy-frontend/src/components/Landing.jsx
+++ study-buddy-frontend/src/components/Landing.jsx
@@ -1,153 +1,408 @@
-import React, { useState } from "react"
+import React, { useRef, useState, useEffect, containerRef } from "react"
 import {
   AppBar,
-  Box,
-  Button,
-  Container,
   Toolbar,
+  Button,
+  Box,
   Typography,
-  useTheme,
+  Container,
+  Grid,
+  Paper,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
 } from "@mui/material"
-import { Link as ScrollLink } from "react-scroll"
-import Login from "./Login"
-import CreateAccount from "./CreateAccount"
-function LandingPage() {
-  const theme = useTheme()
+import { Image as ImageIcon } from "@mui/icons-material"
+import {
+  Star as StarIcon,
+  AccountCircle as AccountCircleIcon,
+} from "@mui/icons-material"
+import dynamic from "next/dynamic"
+import "locomotive-scroll/dist/locomotive-scroll.css"
+// import "@/styles/LocomotiveScroll.module.css"
+const DynamicLocomotiveScroll = dynamic(
+  () => import("locomotive-scroll").then((ls) => ls.default || ls),
+  { ssr: false }
+)
 
-  const [loginOpen, setLoginOpen] = useState(false)
-  const [createAccountOpen, setCreateAccountOpen] = useState(false)
+// theme instance
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#f7f0fa",
+    },
+    primary: {
+      main: "#5813d6",
+      // Dark purple
+    },
+    secondary: {
+      main: "#c3f0c8",
+    },
+    fontColor: {
+      main: "#00000",
+    },
+  },
+})
 
-  const handleOpenLogin = () => setLoginOpen(true)
-  const handleCloseLogin = () => setLoginOpen(false)
+theme.components = {
+  MuiButton: {
+    styleOverrides: {
+      root: {
+        margin: theme.spacing(1),
+      },
+    },
+  },
+}
 
-  const handleOpenCreateAccount = () => setCreateAccountOpen(true)
-  const handleCloseCreateAccount = () => setCreateAccountOpen(false)
+const sections = [
+  { title: "Home", id: "home-section" },
+  { title: "About", id: "about-us-section" },
+  { title: "Potential", id: "unlock-potential-section" },
+  { title: "Customers", id: "customers-section" },
+]
+
+const testimonials = [
+  {
+    review:
+      "Study Buddy is an amazing platform that has helped me improve my study habits and connect with like-minded individuals. Highly recommended!",
+    name: "Wesley Anastasi",
+    role: "Student, Baylor University",
+  },
+  {
+    review:
+      "Study Buddy is an amazing platform that has helped me improve my study habits and connect with like-minded individuals. Highly recommended!",
+    name: "Alex Baratta",
+    role: "Student, Baylor University",
+  },
+  {
+    review:
+      "Study Buddy is an amazing platform that has helped me improve my study habits and connect with like-minded individuals. Highly recommended!",
+    name: "Dr. Song",
+    role: "Professor, Baylor University",
+  },
+]
+
+const LandingPage = () => {
+  const containerRef = useRef(null)
+  const [locoScroll, setLocoScroll] = useState(null)
+
+  useEffect(() => {
+    let locomotiveScroll
+
+    if (containerRef.current) {
+      // Ensure DynamicLocomotiveScroll is loaded before usage
+      import("locomotive-scroll").then((LocomotiveScrollModule) => {
+        const LocomotiveScroll = LocomotiveScrollModule.default
+        locomotiveScroll = new LocomotiveScroll({
+          el: containerRef.current,
+          smooth: true,
+          lerp: 0.1,
+          multiplier: 1, //  speed of the scrolling
+          class: "is-reveal", // the class to add when element is in view
+        })
+        setLocoScroll(locomotiveScroll)
+      })
+    }
+
+    return () => {
+      if (locomotiveScroll) locomotiveScroll.destroy()
+    }
+  }, [])
+  const scrollToSection = (sectionId) => {
+    locoScroll?.scrollTo(`#${sectionId}`, {
+      offset: -100,
+      duration: 1000, // Duration of the scroll animation in milliseconds
+      easing: [0.25, 0.0, 0.35, 1.0], //  easing 
+      disableLerp: true, // disable the lerp effect (smoothing) for this scroll
+    })
+  }
 
   return (
-    <>
-      <AppBar position="fixed" color="default" elevation={0}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Study Buddy
-          </Typography>
-          <Box flexGrow={1} display="flex" justifyContent="flex-end">
-            <ScrollLink to="mission" smooth={true} offset={0} duration={500}>
-              <Button>Our Mission</Button>
-            </ScrollLink>
-            <ScrollLink to="features" smooth={true} offset={0} duration={500}>
-              <Button>Features</Button>
-            </ScrollLink>
-            <ScrollLink
-              to="testimonials"
-              smooth={true}
-              offset={0}
-              duration={500}
-            >
-              <Button>Testimonials</Button>
-            </ScrollLink>
-            <Button
-              onClick={handleOpenLogin}
-              variant="contained"
-              color="primary"
-            >
-              Login
-            </Button>
-            <ScrollLink to="join" smooth={true} offset={0} duration={500}>
+    <div data-scroll-container ref={containerRef}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="sticky" color="primary">
+          <Toolbar>
+            <Typography variant="h6">Logo</Typography>
+            <Box sx={{ flexGrow: 1 }} />
+            {sections.map((section) => (
               <Button
-                variant="contained"
-                color="primary"
-                // onClick={handleOpenCreateAccount}
+                key={section.title}
+                color="inherit"
+                onClick={() => scrollToSection(section.id)}
               >
-                Join Now
+                {section.title}
               </Button>
-            </ScrollLink>
-          </Box>
-        </Toolbar>
-      </AppBar>
+            ))}
+          </Toolbar>
+        </AppBar>
 
-      <Container disableGutters maxWidth={false} sx={{ pt: 0 }}>
+        {/* Home section */}
         <Box
-          id="mission"
+          data-scroll-section
+          id="home-section"
           sx={{
             height: "100vh",
             display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
             flexDirection: "column",
-            backgroundColor: theme.palette.primary.main,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: theme.palette.background.default,
+            color: theme.palette.fontColor.main,
+            textAlign: "center",
+            p: 4,
           }}
         >
-          <Typography variant="h4" gutterBottom>
-            Our Mission
-          </Typography>
-          <Typography textAlign="center">to get 3js to not be the most annoying library to work with </Typography>
-        </Box>
-
-        <Box
-          id="features"
-          sx={{
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-          }}
-        >
-          <Typography variant="h4" gutterBottom>
-            Features
-          </Typography>
-          <Typography textAlign="center">suff go here </Typography>
-        </Box>
-
-        <Box
-          id="testimonials"
-          sx={{
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-            backgroundColor: theme.palette.primary.main,
-          }}
-        >
-          <Typography variant="h4" gutterBottom>
-            Testimonials
-          </Typography>
-          <Typography textAlign="center">
-            "Best study platform ever!" - Not a made up name
-          </Typography>
-        </Box>
-
-        <Box
-          id="join"
-          sx={{
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-          }}
-        >
-          <Typography variant="h4" gutterBottom>
-            Join the Community
-          </Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            onClick={handleOpenCreateAccount}
+          <Typography
+            data-scroll
+            variant="h2"
+            component="h1"
+            gutterBottom
+            sx={{ fontWeight: "bold" }}
           >
-            Sign Up Now
-          </Button>
+            Connect and study with Study Buddy
+          </Typography>
+          <Typography data-scroll variant="h6" sx={{ mb: 4 }}>
+            Study Buddy is the perfect platform to find study partners, schedule
+            meetings, and ace your exams.
+          </Typography>
+          <Box>
+            <Button
+              data-scroll
+              variant="contained"
+              sx={{ bgcolor: "black", "&:hover": { bgcolor: "grey.900" } }}
+            >
+              Get Started
+            </Button>
+            <Button
+              data-scroll
+              variant="outlined"
+              sx={{
+                color: "black",
+                borderColor: "black",
+                "&:hover": { borderColor: "grey.900" },
+              }}
+            >
+              Learn More
+            </Button>
+          </Box>
         </Box>
-      </Container>
 
-      <Login open={loginOpen} onClose={handleCloseLogin} />
-      <CreateAccount
-        open={createAccountOpen}
-        onClose={handleCloseCreateAccount}
-      />
-    </>
+        {/* About Us section */}
+        <Box
+          data-scroll-section
+          id="about-us-section"
+          sx={{
+            height: "100vh",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: theme.palette.background.default,
+            color: theme.palette.fontColor.main,
+            p: 4,
+          }}
+        >
+          <Container maxWidth="lg">
+            <Grid
+              container
+              spacing={8}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Grid item xs={12} md={6}>
+                <Typography
+                  variant="h3"
+                  component="h2"
+                  gutterBottom
+                  data-scroll
+                >
+                  Effortlessly schedule meetings and connect with study buddies
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  data-scroll
+                >
+                  With Study Buddy, you can easily set up meetings with fellow
+                  students, making it convenient to collaborate and learn
+                  together. Join study sessions, exchange ideas, and boost your
+                  academic performance.
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Paper
+                  data-scroll
+                  elevation={0}
+                  sx={{
+                    height: 300,
+                    width: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: "#fff",
+                    border: `1px solid ${theme.palette.primary.main}`,
+                  }}
+                >
+                  <ImageIcon
+                    sx={{ fontSize: 100, color: theme.palette.grey[400] }}
+                  />{" "}
+                  {/* Placeholder for an image */}
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+        <Box
+          data-scroll-section
+          id="unlock-potential-section"
+          sx={{
+            height: "100vh",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: theme.palette.background.main,
+            color: theme.palette.fontColor.main,
+            p: 4,
+          }}
+        >
+          <Container maxWidth="lg">
+            <Grid
+          
+              container
+              spacing={8}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Grid item xs={12} md={6}>
+                <Typography
+                  variant="h4"
+                  component="h3"
+                  gutterBottom
+                  data-scroll
+                >
+                  Unlock Your Potential with Study Buddy
+                </Typography>
+                <Typography variant="subtitle1" gutterBottom data-scroll
+>
+                  Study Buddy is the ultimate tool for effective learning and
+                  networking. With Study Buddy, you can easily set up meetings,
+                  join study sessions, and connect with like-minded individuals.
+                </Typography>
+                <Box sx={{ my: 4 }}>
+                  <Typography variant="h6" component="h4" gutterBottom data-scroll> 
+                    Efficient Learning
+                  </Typography>
+                  <Typography variant="body1" gutterBottom data-scroll>
+                    Collaborate with peers, exchange knowledge, and enhance your
+                    understanding of subjects.
+                  </Typography>
+                  <Typography variant="h6" component="h4" gutterBottom data-scroll>
+                    Network Building
+                  </Typography>
+                  <Typography variant="body1" gutterBottom data-scroll>
+                    Expand your network, make valuable connections, and create
+                    lifelong study buddies.
+                  </Typography>
+                  <Button data-scroll variant="contained" sx={{ mr: 2 }}>
+                    Learn More
+                  </Button>
+                  <Button data-scroll variant="outlined">Sign Up</Button>
+                </Box>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Paper
+                  elevation={0}
+                  data-scroll
+                  sx={{
+                    height: 300,
+                    width: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: "#fff",
+                    border: `1px solid ${theme.palette.primary.main}`,
+                  }}
+                >
+                  <ImageIcon
+                    sx={{ fontSize: 100, color: theme.palette.grey[400] }}
+                  />{" "}
+                  {/* Placeholder for an image */}
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+
+        <Box
+          data-scroll-section
+          id="customers-section"
+          sx={{
+            height: "100vh",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: theme.palette.background.default,
+            color: theme.palette.fontColor.main,
+            p: 4,
+          }}
+        >
+          <Container maxWidth="lg">
+            <Typography
+              variant="h4"
+              component="h2"
+              gutterBottom
+              align="center"
+              data-scroll
+            >
+              Happy Customers
+            </Typography>
+            <Typography variant="subtitle1" align="center" sx={{ mb: 4 }}>
+              Read what our satisfied users have to say about Study Buddy.
+            </Typography>
+            <Grid container spacing={4} justifyContent="center">
+              {/* Map this Grid item for each testimonial */}
+              {testimonials.map((testimonial, index) => (
+                <Grid item xs={12} sm={4} key={index}>
+                  <Box sx={{ textAlign: "center" }}>
+                    <Box
+                      sx={{
+                        mb: 1,
+                        display: "inline-flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      {[...Array(5)].map((_, i) => (
+                        <StarIcon
+                          key={i}
+                          sx={{ color: theme.palette.secondary.main }}
+                        /> 
+                      ))}
+                    </Box>
+                    <Typography variant="body1" sx={{ mb: 2 }}>
+                      {testimonial.review}
+                    </Typography>
+                    <AccountCircleIcon
+                      sx={{ fontSize: 60, color: theme.palette.grey[400] }}
+                    />
+                    <Typography variant="subtitle2">
+                      {testimonial.name}
+                    </Typography>
+                    <Typography variant="caption">
+                      {testimonial.role}
+                    </Typography>
+                  </Box>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </Box>
+
+        {/* footer */}
+      </ThemeProvider>
+    </div>
   )
 }
 
--- study-buddy-frontend/src/pages/index.jsx
+++ study-buddy-frontend/src/pages/index.jsx
@@ -9,7 +9,6 @@ import {
   TextField,
 } from "@mui/material"
 import styles from "@/styles/Home.module.css"
-import landingstyles from "@/styles/landing.module.css"
 import Header from "@/components/Header.jsx"
 import LandingPage from "@/components/Landing"
 import Sidebar from "@/components/Sidebar.jsx"
@@ -31,35 +30,3 @@ function landing() {
 }
 
 export default landing
-
-// export default function HomePage() {
-//   const onButtonPress = () => {
-//     alert('You pressed a button!');
-//   }
-
-//   return (
-//     <>
-//       <Head>
-//         <title>Home Page</title>
-//       </Head>
-
-//       <main>
-//         <Stack sx={{ paddingTop: 4 }} alignItems='center' gap={2}>
-//           <Card sx={{ width: 600 }} elevation={4}>
-//             <CardContent>
-//               <Typography variant='h3' align='center'>Study Buddy Spring 2024</Typography>
-//               <Typography variant='body1' color='text.secondary'>This is your template project for the Spring 2024 Baylor Software Engineering II class project! See the README for insturctions on how to set this project up and run it locally.</Typography>
-//             </CardContent>
-//           </Card>
-//           <Stack direction="row">
-//             {/* There are multiple ways to apply styling to Material UI components. One way is using the `sx` prop: */}
-//             <Button variant='contained' onClick={onButtonPress} sx={{ width: 200 }}>I am a button</Button>
-
-//             {/* Another way is by creating a dedicated CSS file and using the styles from there: */}
-//             <Button variant='contained' color="secondary" onClick={onButtonPress} className={styles.wideButton}>I am a wider button</Button>
-//           </Stack>
-//         </Stack>
-//       </main>
-//     </>
-//   );
-// }
