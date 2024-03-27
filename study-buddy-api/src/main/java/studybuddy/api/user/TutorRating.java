cf5b4ad18f45a3ed63d1ba7a86f0a0ba008f4deb
Gabriel Choi <gabechoi03@gmail.com>
2024-03-27 13:17:13 -0500

    Added tutor_rating table to database

---
+++ study-buddy-api/src/main/java/studybuddy/api/user/TutorRating.java
@@ -0,0 +1,25 @@
+package studybuddy.api.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import studybuddy.api.meeting.Meeting;
+import studybuddy.api.user.User;
+
+@Data
+@Entity
+@Table(name = studybuddy.api.user.TutorRating.TABLE_NAME)
+public class TutorRating {
+    public static final String TABLE_NAME = "TUTOR_RATING";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TUTOR_RATING_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID", referencedColumnName = "USER_ID", foreignKey = @ForeignKey(name = "FK_MEETING_USER_ID"))
+    private User user;
+
+    @Column(name = "RATING")
+    int rating;
+}
