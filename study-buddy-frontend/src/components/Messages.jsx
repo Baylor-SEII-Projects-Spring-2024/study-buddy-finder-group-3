0933fc8b63284e8a65e680b832e1b03742e220b6
Spammck <108024146+Spammck@users.noreply.github.com>
2024-04-02 12:42:53 -0500

    removed photos

--- study-buddy-frontend/src/components/Messages.jsx
+++ study-buddy-frontend/src/components/Messages.jsx
@@ -1,14 +1,14 @@
 import React from "react"
 import styles from "@/styles/Chat.module.css";
 import Message from "./Message"
+
 const Messages = () => {
   return (
     <div className={styles.Messages}>
-      <Message isOwner={true}/>
-      <Message isOwner={false}/>
-      <Message/>
-      <Message/>
-      <Message/>
+      <Message isOwner={true}><p>This is an example</p></Message>
+      <Message isOwner={false} />
+      <Message isOwner={true} />
+      <Message />
     </div>
   )
 }
