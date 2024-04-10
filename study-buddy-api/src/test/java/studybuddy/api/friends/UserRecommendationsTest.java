82de75d55828a90bc421c68edf1ce8697a50751f
Gabriel Choi <gabechoi03@gmail.com>
2024-04-10 13:06:03 -0500

    made recommendations separate for users and tutors

---
+++ study-buddy-api/src/test/java/studybuddy/api/friends/UserRecommendationsTest.java
@@ -0,0 +1,34 @@
+package studybuddy.api.friends;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import studybuddy.api.endpoint.AuthEndpoint;
+import studybuddy.api.endpoint.UserEndpoint;
+import studybuddy.api.meeting.meetingService;
+import studybuddy.api.user.User;
+import studybuddy.api.user.UserRepository;
+import studybuddy.api.user.UserService;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@ActiveProfiles("testdb")
+@Transactional
+public class UserRecommendationsTest {
+    
+}
