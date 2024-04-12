7ad5f3db58e8ee1c8089e4939adcd03e10781884
wesley <anastasicrl@gmail.com>
2024-04-12 16:08:43 -0500

    fix occasional crashes

--- study-buddy-frontend/src/components/FriendsPage/FriendsHeader.jsx
+++ study-buddy-frontend/src/components/FriendsPage/FriendsHeader.jsx
@@ -1,57 +1,65 @@
-import * as React from 'react';
+import * as React from "react"
 import axios from "axios"
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from "@mui/material/AppBar"
+import Box from "@mui/material/Box"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+import IconButton from "@mui/material/IconButton"
+import MenuIcon from "@mui/icons-material/Menu"
 import { useRouter } from "next/router"
 import { useSelector, useDispatch } from "react-redux"
 import { selectToken, setToken, logout } from "@/utils/authSlice.js"
 
 export default function FriendsHeader() {
-    const router = useRouter();
-    const dispatch = useDispatch()
+  const router = useRouter()
+  const dispatch = useDispatch()
 
-    const navigateToProfile = () => {
-        router.push("/profile")
-      }
-    
-      const navigateFriends = () => {
-        router.push("/friends")
-      }
-    
-      const navigateHome = () => {
-        router.push("/home")
-      }
+  const navigateToProfile = () => {
+    router.push("/profile")
+  }
 
-      const handleLogout = async () => {
-        localStorage.removeItem("token")
-        dispatch(logout());
-        router.push("/")
-      }
+  const navigateFriends = () => {
+    router.push("/friends")
+  }
 
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1}}
-            style={{boxShadow: "none", borderBottom: "1px solid black", backgroundColor: '#f7f0fa'}}>
-                <Toolbar>
-                    <Button onClick={navigateHome}>Home</Button>
+  const navigateHome = () => {
+    router.push("/home")
+  }
 
-                    <Button>Meetings</Button>
+  const handleLogout = async () => {
+    localStorage.removeItem("token")
+    dispatch(logout())
+    router.push("/")
+  }
 
-                    <Button>Settings</Button>
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        style={{
+          boxShadow: "none",
+          borderBottom: "1px solid black",
+          backgroundColor: "#f7f0fa",
+        }}
+      >
+        <Toolbar>
+          <Button onClick={navigateHome}>Home</Button>
 
-                    <Typography sx={{ flexGrow: 1, color: 'black', textAlign: 'center' }}>
-                      Logo
-                    </Typography>
+          <Button>Meetings</Button>
 
-                    <Button onClick={handleLogout} variant='contained'>Menu</Button>
+          <Button>Settings</Button>
 
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+          <Typography sx={{ flexGrow: 1, color: "black", textAlign: "center" }}>
+            Logo
+          </Typography>
+
+          <Button onClick={handleLogout} variant="contained">
+            Menu
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
 }
--- study-buddy-frontend/src/components/FriendsPage/FriendsRequests.jsx
+++ study-buddy-frontend/src/components/FriendsPage/FriendsRequests.jsx
@@ -1,95 +1,104 @@
 import React, { useState, useEffect } from "react"
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box"
 import { useSelector } from "react-redux"
 import { selectToken, selectUser } from "@/utils/authSlice.js"
 import { useRouter } from "next/router"
 import axios from "axios"
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Typography from '@mui/material/Typography';
-import Button from "@mui/material/Button";
-import { API_URL } from "@/utils/config";
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+import ListItemText from "@mui/material/ListItemText"
+import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+import { API_URL } from "@/utils/config"
 
-export default function FriendsRequest( {onUpdate}) {
-
-
+export default function FriendsRequest({ onUpdate }) {
   const token = useSelector(selectToken)
   const user = useSelector(selectUser)
   const router = useRouter()
-  const [friends, setFriendsList] = useState([]);
-  const [userId, setUserid] = useState('')
+  const [friends, setFriendsList] = useState([])
+  const [userId, setUserid] = useState("")
 
   useEffect(() => {
     if (!token || !user) {
-      router.push('/');
+      router.push("/")
     }
-  }, [token, router]);
+  }, [token, router])
 
   useEffect(() => {
-    if (user){
-      console.log('here')
+    if (user) {
+      console.log("here")
       setUserid(user.id)
     }
     fetchAllInfo()
   }, [user])
-  
+
   const fetchAllInfo = async () => {
     try {
-      const response = await axios.get(`${API_URL}/friends/${user.id}/getRequests`);
-      setFriendsList(response.data);
+      const response = await axios.get(
+        `${API_URL}/friends/${user.id}/getRequests`
+      )
+      setFriendsList(response.data)
     } catch (error) {
-      console.error("Error fetching friends info:", error);
+      console.error("Error fetching friends info:", error)
     }
   }
 
   const removeRequest = (user1) => {
     try {
-      axios.post(`${API_URL}/friends/${user1.id}/delete/${user.id}`)
-       .then(response => {
-         console.log(response);
-      })
+      axios
+        .post(`${API_URL}/friends/${user1.id}/delete/${user.id}`)
+        .then((response) => {
+          console.log(response)
+        })
     } catch (error) {
       console.error("Error removing request:", error)
     }
-    const updatedFriends = friends.filter((user2) => user2.id !== user1.id);
-    setFriendsList(updatedFriends);
-    onUpdate();
+    const updatedFriends = friends.filter((user2) => user2.id !== user1.id)
+    setFriendsList(updatedFriends)
+    onUpdate()
   }
 
   const handleListItemClick = (event, user2) => {
     try {
-      axios.post(`${API_URL}/friends/${user.id}/add/${user2.id}`)
-       .then(response => {
-         console.log(response);
-      })
+      axios
+        .post(`${API_URL}/friends/${user.id}/add/${user2.id}`)
+        .then((response) => {
+          console.log(response)
+        })
     } catch (error) {
       console.error("Error adding friend:", error)
     }
-    removeRequest(user2);
+    removeRequest(user2)
   }
-
-
 
   return (
     <div>
       {friends.length === 0 ? (
-        <Typography variant="h3" padding={20} style={{textAlign: "center"}} color={"gray"}>
+        <Typography
+          variant="h3"
+          padding={20}
+          style={{ textAlign: "center" }}
+          color={"gray"}
+        >
           No new requests.
         </Typography>
       ) : (
-        <Box sx={{ flexGrow: 1, maxWidth: 752 }}> 
+        <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
           <List>
-            {friends.map(user => (
+            {friends.map((user) => (
               <ListItem key={user.id}>
-                    <ListItemText primary={user.username} />
-                    <Button onClick={(event) => handleListItemClick(event, user)}>Accept</Button>
-                    <Button onClick={(event) => removeRequest(user)}>Decline</Button>
+                <ListItemText primary={user.username} />
+                <Button onClick={(event) => handleListItemClick(event, user)}>
+                  Accept
+                </Button>
+                <Button onClick={(event) => removeRequest(user)}>
+                  Decline
+                </Button>
               </ListItem>
             ))}
           </List>
         </Box>
       )}
     </div>
-  );
+  )
 }
