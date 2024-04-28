33cff0122d47690a49bd3c11770ceaa3eceb81ef
AlexBaratta <alexbaratta1@gmail.com>
2024-04-27 23:53:58 -0500

    fix settings merge

--- study-buddy-api/src/main/java/studybuddy/api/endpoint/CoursesEndpoint.java
+++ study-buddy-api/src/main/java/studybuddy/api/endpoint/CoursesEndpoint.java
@@ -1,70 +1,42 @@
 package studybuddy.api.endpoint;
 
-import lombok.extern.log4j.Log4j2;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
 import studybuddy.api.courses.Courses;
-import studybuddy.api.courses.CoursesService;
-import studybuddy.api.utils.JwtUtil;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-@Log4j2
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import studybuddy.api.utils.JwtUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/courseInfo")
 public class CoursesEndpoint {
     @Autowired
-    private CoursesService coursesService;
-
+    private JwtUtil jwtUtil;
     @Autowired
     JdbcTemplate jdbcTemplate;
-
-    @GetMapping("/names")
-    public ResponseEntity<List<Courses>> getCoursesBySearchTerm(@RequestParam(required = false) String search) {
-        log.info("Search term received: '{}'", search);
-        List<Courses> courses;
-        if (search == null || search.isEmpty()) {
-            courses = coursesService.getAllCourses();
-            log.info("Returning all courses.");
-        } else {
-            courses = coursesService.getCoursesContaining(search);
-            log.info("Returning filtered courses.");
-        }
-        log.info("Number of courses found: {}", courses.size());
-        return ResponseEntity.ok(courses);
-    }
 
     @GetMapping("/user/{userId}/courses")
     public ResponseEntity<List<Courses>> getCoursesByUserId(@PathVariable Long userId) {
         List<Courses> courses = new ArrayList<>();
-        /*List<Courses> courses = jdbcTemplate.query(sql, new Object[]{userId}, (rs, rowNum) ->
-                        new Courses(
-                                rs.getLong("course_id"),
-                                rs.getString("c_name"),
-                                rs.getString("description"),
-                                rs.getString("subject_area")
-                        )
-        );*/
-
-        /*
-        List<Courses> courses = jdbcTemplate.query(sql, new Object[]{userId}, new CourseRowMapper());
-        for(Courses c: courses) {
-            System.out.println(c.getName());
-        }
-        return new ResponseEntity<>(courses, HttpStatus.OK);
-*/
-
         try {
             // List<Courses> courses = new ArrayList<>();
             String sql = "SELECT * FROM courses " +
@@ -113,16 +85,7 @@ public class CoursesEndpoint {
             jdbcTemplate.update(
                     "INSERT INTO usercourses (course_id, istutored, user_id) VALUES(?, ?, ?)",
                     c.getId(),false,userId);
-            /*
-            List<Object[]> parameters = new ArrayList<>();
-            parameters.add(new Object[]{
-                    c.getId(),
-                    false,
-                    userId
-            });
-            jdbcTemplate.batchUpdate(
-                    "INSERT INTO usercourses (course_id, istutored, user_id) VALUES(?, ?, ?)",
-                    parameters);*/
+
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } catch (Exception e) {
             //return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to get courses for user");
@@ -148,12 +111,6 @@ public class CoursesEndpoint {
             Long courseId = keyHolder.getKey().longValue();
             return ResponseEntity.status(HttpStatus.CREATED).body(courseId);
 
-
-            /*
-            jdbcTemplate.update(
-                    "INSERT INTO courses (c_description, c_name, subject_area) VALUES (?, ?, ?)",
-                    c.getDescription(), c.getName(), c.getSubjectArea());
-            return ResponseEntity.status(HttpStatus.CREATED).build();*/
         } catch (Exception e) {
             //return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to get courses for user");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
@@ -233,11 +190,14 @@ public class CoursesEndpoint {
     public boolean addFriendRequest(@PathVariable Long idTo, @PathVariable Long idFrom)
     {
         List<Object[]> parameters = new ArrayList<>();
+
         parameters.add(new Object[]{
                 idTo,
                 idFrom
         });
+
         jdbcTemplate.batchUpdate("INSERT INTO friends_request (userto_id, userfrom_id) VALUES(?, ?)", parameters);
+
         return true;
     }*/
 
@@ -255,4 +215,6 @@ public class CoursesEndpoint {
             return course;
         }
     }
+
+
 }
