9736589fc9072623dde9ad520fa7372849d9971e
Spammck <108024146+Spammck@users.noreply.github.com>
2024-05-02 15:22:33 -0500

    made passing chats test

--- study-buddy-api/src/test/java/studybuddy/api/chat/ChatTests.java
+++ study-buddy-api/src/test/java/studybuddy/api/chat/ChatTests.java
@@ -1,13 +1,11 @@
 package studybuddy.api.chat;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import studybuddy.api.user.Messages;
-import studybuddy.api.user.User;
-import studybuddy.api.user.UserService;
-import studybuddy.api.user.messageService;
+import studybuddy.api.user.*;
 
 import java.util.List;
 
@@ -23,17 +21,19 @@ class ChatTests {
     @Autowired
     UserService userService;
 
-    @Test
-    void emptyTest() {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @AfterEach
+    void tearDown() throws Exception {
+        messageRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
-    void exampleTest() {
-        assertEquals(2, 1 + 1);
-    }
-
-    @Test
-    void createMessagePass() {
+    void createMessagePass() throws Exception {
         User user = new User();
         User newUser = new User();
 
@@ -53,5 +53,58 @@ class ChatTests {
         assertEquals(1, msgs.size());
         Messages msg = msgs.get(0);
         assertEquals(msg.getContent(), "Test Message");
+
+    }
+
+    @Test
+    void createMessagesPass() throws Exception {
+        User user = new User();
+        User newUser = new User();
+
+        user.setUsername("test1");
+        newUser.setUsername("test2");
+
+        user.setId(1L);
+        newUser.setId(2L);
+
+        userService.saveUser(user);
+        userService.saveUser(newUser);
+
+        messageService.sendMessage(user,newUser,"Test Message1");
+        messageService.sendMessage(user,newUser,"Test Message2");
+        messageService.sendMessage(user,newUser,"Test Message3");
+
+        List<Messages> msgs = messageService.findMessageByUsers(user.getId(),newUser.getId());
+
+        assertEquals(3, msgs.size());
+        for(int i = 0; i < 3; i++) {
+            Messages msg = msgs.get(i);
+            assertEquals(msg.getContent(), "Test Message" + (i+1));
+        }
+    }
+
+    @Test
+    void createMessageFail() throws Exception {
+        User user = new User();
+        User newUser = new User();
+
+        user.setUsername("test1");
+        newUser.setUsername("test2");
+
+        user.setId(1L);
+        newUser.setId(2L);
+
+        userService.saveUser(user);
+        userService.saveUser(newUser);
+
+        messageService.sendMessage(user,newUser,"Test Message");
+
+        List<Messages> msgs = messageService.findMessageByUsers(user.getId(),newUser.getId());
+
+        assertEquals(1, msgs.size());
+        Messages msg = msgs.get(0);
+
+        assertEquals(msg.getUser(), newUser);
+        assertEquals(msg.getReceiver(), user);
     }
 }
